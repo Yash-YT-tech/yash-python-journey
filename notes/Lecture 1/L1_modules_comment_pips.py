90de25561b5bf17d18a49e5bd78fbee921518e38
Yash Tathe <yashtathe0@gmail.com>
2026-02-22 22:00:44 +0530

    modules_pips_comments_notes
    add lecture notes and examples for python modules, pip and comments

---
+++ notes/Lecture 1/L1_modules_comment_pips.py
@@ -0,0 +1,7 @@
+# this is the pyjokes module we have installed with the help of
+
+import pyjokes as jokes
+
+print(jokes.get_joke())
+
+# Comments :- Single line  # Single line , Multi Line ''' multi line '''
