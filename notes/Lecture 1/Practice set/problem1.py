5c1b795b2fe0f477bc9459b61d22185d59ace445
Yash Tathe <yashtathe0@gmail.com>
2026-02-22 22:03:37 +0530

    add lecture 1 problem 1 solution
    Added first practice problem for Lecture 1 covering modules, pip usage and basic comments.

---
+++ notes/Lecture 1/Practice set/problem1.py
@@ -0,0 +1,12 @@
+# Problem 1 :- Write twinkle - twinkle litte star poem in python
+
+# By single print statement
+# print("Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you are!")
+
+# By multiple print statement
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
