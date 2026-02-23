1e09dc38d7392c7d3ed5b8ff2a5840cc32efbe64
Yash Tathe <yashtathe0@gmail.com>
2026-02-23 21:24:42 +0530

    add lecture 1 problem 3 solution

---
+++ notes/Lecture 1/Practice set/Problem3.py
@@ -0,0 +1,8 @@
+
+# Problem 3 :- Install an external module and use it 
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello, my name is yash i am a python programmer")
+engine.runAndWait()
+
