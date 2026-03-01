ae794e90cfd1ba4f7b3725ad8541a27266a119b1
Yash Tathe <yashtathe0@gmail.com>
2026-03-01 09:12:03 +0530

    Add solution for Lecture 2 - Problem 5

---
+++ notes/Lecture 2/Practice set/problem5.py
@@ -0,0 +1,8 @@
+# Problem 5 : Write a python program to find an average of two numbers entered by the user 
+
+num_1 = int(input("Enter first number :"))
+num_2 = int(input("Enter second number :"))
+
+average = (num_1 + num_2) / 2 
+
+print("The average of the given numbers are :" , average)
