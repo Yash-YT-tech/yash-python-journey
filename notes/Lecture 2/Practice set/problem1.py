70b98c08ac0ff85e7b45a7a3523116fce1f31008
Yash Tathe <yashtathe0@gmail.com>
2026-03-01 09:10:00 +0530

    Add solution for Lecture 2 - Problem 1

---
+++ notes/Lecture 2/Practice set/problem1.py
@@ -0,0 +1,21 @@
+# Problem 1 : Make a programm to add two numbers 
+
+num_1 = int(input("Enter your 1st Number: "))
+num_2 = int(input("Enter your 2st Number: "))
+
+sum = num_1 + num_2
+
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
