6a20ad41b8a717436864069028d7af5d52ea2619
Yash Tathe <yashtathe0@gmail.com>
2026-03-01 09:12:30 +0530

    Add solution for Lecture 2 - Problem 6

---
+++ notes/Lecture 2/Practice set/problem6.py
@@ -0,0 +1,6 @@
+# Problem 6 : Write a python programm to calculate the square of a number entered by the user 
+
+num = int(input("Ener number to be squared : "))
+square = num**2
+
+print("Square of the given number is :",square)
