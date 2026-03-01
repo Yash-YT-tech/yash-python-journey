1ae81d4a4610b74d278520102961341dc5f0a9c5
Yash Tathe <yashtathe0@gmail.com>
2026-03-01 09:10:27 +0530

    Add solution for Lecture 2 - Problem 2

---
+++ notes/Lecture 2/Practice set/problem2.py
@@ -0,0 +1,8 @@
+# Problem 2 : Write a python program to find remainder when a number is divided by z
+
+num_1 = int(input("Enter number :"))
+z = int(input("Enter number to divide : "))
+
+remainder = num_1 % z
+# print("when" num_1 "is divided be" z "the remainder is : ", remainder)
+print("The remainder is :",remainder)
