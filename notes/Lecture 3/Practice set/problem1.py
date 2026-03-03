322eb50eafd4052e2f7524cc662bb59fb8566d61
Yash Tathe <yashtathe0@gmail.com>
2026-03-03 10:18:34 +0530

    Add solution for Lecture 3- Problem 1

---
+++ notes/Lecture 3/Practice set/problem1.py
@@ -0,0 +1,6 @@
+# Problem 1 : Write a python program to display a user entered name followed by Good afternoon using input() fucntionn
+
+name = input("Enter your name : ")
+uppercase = name.upper()
+capital = name.capitalize()
+print(f"Good Afternoon , {capital} ")
