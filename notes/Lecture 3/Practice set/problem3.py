3b6a890a284a603e10784605c6de1fec2001b035
Yash Tathe <yashtathe0@gmail.com>
2026-03-03 10:19:36 +0530

    Add solution for Lecture 3- Problem 3

---
+++ notes/Lecture 3/Practice set/problem3.py
@@ -0,0 +1,5 @@
+# Problem 3 : Write a program to detect double space in string
+
+sentence = "hi my name is  Yash"
+print(sentence.find("  ")) # if the argument is not present in the string the find function will return (-1)
+
