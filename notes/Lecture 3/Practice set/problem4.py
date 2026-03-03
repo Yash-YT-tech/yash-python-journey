0a32884a21100a1439ebf4289d66fdb3d47f6f43
Yash Tathe <yashtathe0@gmail.com>
2026-03-03 10:19:56 +0530

    Add solution for Lecture 3- Problem 4

---
+++ notes/Lecture 3/Practice set/problem4.py
@@ -0,0 +1,7 @@
+# Problem 4: Replace the double space from problem 3 with single space 
+
+sentence = "hi my name is  Yash"
+print(sentence.replace("  "," "))
+
+# immutability of the string : string are immutable , the string functions doesn't  change the orignal string , it recreate another string and apply the arguments to it and print us that string 
+# this is the basic logic of the immutability of the strings 
