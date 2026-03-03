aba3ea6698cca78028576c824dce858019e72b8c
Yash Tathe <yashtathe0@gmail.com>
2026-03-03 10:23:28 +0530

    list_notes
    Add list notes

---
+++ notes/Lecture 4/list.py
@@ -0,0 +1,16 @@
+# list: 
+# -are mutable
+# -Multiple datatypes assainged 
+# -we can assa
+
+list = [1 , "Yash" , True , False , "World is slow" , "Bro you are"]
+
+# print(list.append())
+
+# list[4] = "World is fast" #the changes are applied in the original list 
+
+# print(list) # it prints the list with the changes 
+
+## List slicing 
+
+print(list[3:4])
