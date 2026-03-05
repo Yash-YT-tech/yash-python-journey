933d67b10203c8eac35f23b50bc4f5819bb78d6e
Yash Tathe <yashtathe0@gmail.com>
2026-03-05 21:40:11 +0530

    Add solution for Lecture 4- Problem 1

---
+++ notes/Lecture 4/Practice set/problem1.py
@@ -0,0 +1,26 @@
+# Problem 1 : Write a program to store seven fruits in a list entered by the user 
+
+fruits = []
+
+first_fruit = input("Enter the name of the first fruit: ") 
+fruits.append(first_fruit)
+
+second_fruit = input("Enter the name of the second fruit: ") 
+fruits.append(second_fruit)
+
+third_fruit = input("Enter the name of the third fruit: ") 
+fruits.append(third_fruit)
+
+fourth_fruit = input("Enter the name of the fourth fruit: ") 
+fruits.append(fourth_fruit)
+
+fifth_fruit = input("Enter the name of the fifth fruit: ") 
+fruits.append(fifth_fruit)
+
+sixth_fruit = input("Enter the name of the sixth fruit: ") 
+fruits.append(sixth_fruit)
+
+seventh_fruit = input("Enter the name of the seventh fruit: ") 
+fruits.append(seventh_fruit)
+
+print(fruits)
