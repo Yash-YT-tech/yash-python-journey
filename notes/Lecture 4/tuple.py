a83bc19f6679154de6f9e58de39b20a3d47ef220
Yash Tathe <yashtathe0@gmail.com>
2026-03-05 21:38:02 +0530

    tuple_notes
    add tuple notes

---
+++ notes/Lecture 4/tuple.py
@@ -0,0 +1,17 @@
+# Tuples 
+# -are immutable
+# -Multiple data types
+
+tuple = ("yash",1,"GOD",23,"rain")
+
+print(type(tuple))
+
+# If we want to make and single element tuple we have to put an comma after the particular element/index 
+
+single_tuple = ("Yash",) # Without comma(,) it will be consider as string and not and tuple
+print(type(single_tuple))
+
+# The following is the way to create an empty tuple
+
+empty_tuple = ()
+print(type(empty_tuple))
