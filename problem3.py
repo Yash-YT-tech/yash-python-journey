6b8fe47688550b92226a31f96b7483127c0f1fbe
Yash Tathe <yashtathe0@gmail.com>
2026-03-06 19:15:59 +0530

    Delete problem3.py

--- problem3.py
+++
@@ -1,7 +0,0 @@
-# Problem 3 : Check that a tuple type cannot be changed in python 
-
-tuple = (1,"Yash",22)
-tuple[2] = "King"
-# Hence proved that tuple type is immutable in python 
-print(tuple)
-
