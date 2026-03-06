4803c08a2cb515f3baf1f5d0087ac5191f16e4d1
Yash Tathe <yashtathe0@gmail.com>
2026-03-06 19:13:11 +0530

    Delete notes/Lecture 5/Practice set/problem 3.py

--- notes/Lecture 5/Practice set/problem 3.py
+++
@@ -1,7 +0,0 @@
-# Problem 3 : Can we have a set with 18 (int) and '18' (str) as a value in it?
-
-check_set_values = {18,"18"}
-
-print(check_set_values)
-
-# therefore , yes we can have it and its valid bro  
