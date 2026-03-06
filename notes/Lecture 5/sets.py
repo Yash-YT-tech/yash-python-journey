cb560f5db2579f4177bcad49f68d69873c2680e3
Yash Tathe <yashtathe0@gmail.com>
2026-03-06 19:01:43 +0530

    sets_notes
    add sets notes

---
+++ notes/Lecture 5/sets.py
@@ -0,0 +1,19 @@
+# Sets 
+# unique value (no repetations allowed)
+# 1. Sets are unordered => Element’s order doesn’t matter
+# 2. Sets are unindexed => Cannot access elements by index
+# 3. There is no way to change items in sets.
+# 4. Sets cannot contain duplicate values.
+
+empty_set = set()
+print(type(empty_set))
+
+set = {1,1,2,3 ,34,5543,4}
+print(set) 
+# the above set will return {1,2,3} as the contains only unqiue values
+# 
+# 
+
+set1 = { 1, "Yash","yash", 232.32 ,} # only string , integers and decimals 
+print(set1)
+
